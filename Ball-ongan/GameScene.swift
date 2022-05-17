6f98ac240ceb729deefbad54a2d999e5551186b6
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-05-17 16:57:34 +0800

    Added player, point, obstacle to game scene

--- Ball-ongan/GameScene.swift
+++ Ball-ongan/GameScene.swift
@@ -32,6 +32,55 @@ class GameScene: SKScene {
         background.size = CGSize(width: screenWidth(), height: screenHeight())
         addChild(background)
         
+        createPlayer()
+        createPoint()
+        createObstacle()
+        
+    }
+    
+    func createPlayer() {
+        let sprite = SKSpriteNode(imageNamed: "player")
+        sprite.position = CGPoint(x: -screenWidth()/4, y: 0)
+        sprite.size = CGSize(width: 50, height: 50)
+        sprite.name = "player"
+        sprite.zPosition = 1
+        addChild(sprite)
+    }
+    
+    func createPoint() {
+        
+        let pointsTotal = level + 2
+        
+        let randomDistributionX = GKRandomDistribution(lowestValue: -Int(screenWidth()/2 - 75), highestValue: Int(screenWidth()/2 - 50))
+        let randomDistributionY = GKRandomDistribution(lowestValue: -Int(screenHeight()/2 - 30), highestValue: Int(screenHeight()/2 - 30))
+        
+        for _ in 0..<pointsTotal {
+            let sprite = SKSpriteNode(imageNamed: "point")
+            sprite.position = CGPoint(x: randomDistributionX.nextInt(), y: randomDistributionY.nextInt())
+            sprite.size = CGSize(width: 25, height: 25)
+            sprite.name = "point"
+            sprite.zPosition = 1
+            addChild(sprite)
+        }
+        
+    }
+    
+    func createObstacle() {
+        
+        let obstaclesTotal = 5 + (level * 2)
+        
+        let randomDistributionX = GKRandomDistribution(lowestValue: -Int(screenWidth()/2 - 75), highestValue: Int(screenWidth()/2 - 50))
+        let randomDistributionY = GKRandomDistribution(lowestValue: -Int(screenHeight()/2 - 30), highestValue: Int(screenHeight()/2 - 30))
+        
+        for _ in 0..<obstaclesTotal {
+            let sprite = SKSpriteNode(imageNamed: "obstacle")
+            sprite.position = CGPoint(x: randomDistributionX.nextInt(), y: randomDistributionY.nextInt())
+            sprite.size = CGSize(width: 25, height: 25)
+            sprite.name = "obstacle"
+            sprite.zPosition = 1
+            addChild(sprite)
+        }
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
